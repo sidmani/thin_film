fix broadcasting issues

thin_film/__main__.py
@@ -33,11 +33,11 @@ def run(particle_count, steps, constants, workers):
     ) + np.array([bounds[0], bounds[1]])
     u = np.zeros((particle_count, 2))
     # surfactant concentration (Γ)
-    Gamma = np.zeros((particle_count, 1))
+    Gamma = np.zeros((particle_count,))
     # numerical and advected height
     # TODO: num_h and adv_h are off by a few orders of magnitude
-    num_h = np.ones((particle_count, 1)) * constants.h_0
-    adv_h = np.ones((particle_count, 1)) * constants.h_0
+    num_h = np.ones((particle_count,)) * constants.h_0
+    adv_h = np.ones((particle_count,)) * constants.h_0
 
     frames = []
     frame_pbar = tqdm(total=steps, position=1, leave=True)
@@ -58,11 +58,9 @@ def run(particle_count, steps, constants, workers):
 
     frames.sort(key=lambda x: x[1])
     frames = list(map(lambda x: x[0], frames))
-    im1 = plt.imshow(frames[0])
 
-    def update(i):
-        im1.set_data(frames[i])
-    ani = FuncAnimation(plt.gcf(), update, interval=200)
+    im1 = plt.imshow(frames[0])
+    ani = FuncAnimation(plt.gcf(), func=lambda f: im1.set_data(f), frames=frames, interval=200,)
     plt.show()
 
 

thin_film/color_system.py
@@ -1,10 +1,12 @@
-# colour_system.py
 import numpy as np
 
 cmf = np.loadtxt("cie-cmf.txt", usecols=(1, 2, 3))
+
+
 def xyz_from_xy(x, y):
     """Return the vector (x, y, 1-x-y)."""
     return np.array((x, y, 1-x-y))
+
 
 class ColorSystem:
     # The CIE colour matching function for 380 - 780 nm in 5 nm intervals
@@ -22,7 +24,7 @@ class ColorSystem:
         self.red, self.green, self.blue = red, green, blue
         self.white = white
         # The chromaticity matrix (rgb -> xyz) and its inverse
-        self.M = np.vstack((self.red, self.green, self.blue)).T 
+        self.M = np.vstack((self.red, self.green, self.blue)).T
         self.MI = np.linalg.inv(self.M)
         # White scaling array
         self.wscale = self.MI.dot(self.white)
@@ -59,8 +61,9 @@ class ColorSystem:
     #         return XYZ
     #     return XYZ / den
 
+
 illuminant_D65 = xyz_from_xy(0.3127, 0.3291)
 cs_srgb = ColorSystem(red=xyz_from_xy(0.64, 0.33),
-                       green=xyz_from_xy(0.30, 0.60),
-                       blue=xyz_from_xy(0.15, 0.06),
-                       white=illuminant_D65)
+                      green=xyz_from_xy(0.30, 0.60),
+                      blue=xyz_from_xy(0.15, 0.06),
+                      white=illuminant_D65)

thin_film/physics.py
@@ -16,7 +16,8 @@ def compute_surfactant_diffusion(
     V, num_h, Gamma, Gamma_i, alpha_c, delta_t, grad_kernel_reduced
 ):
     return Gamma_i + (
-        alpha_c * delta_t * np.sum(V / num_h * (Gamma - Gamma_i) * grad_kernel_reduced)
+        alpha_c * delta_t *
+        np.sum(V / num_h * (Gamma - Gamma_i) * grad_kernel_reduced)
     )
 
 
@@ -36,7 +37,7 @@ def pressure_force(V, num_h, pressure, num_h_i, pressure_i, grad_kernel):
         * V**2
         * np.sum(
             num_h_i
-            * (pressure_i / num_h_i ** 2 + pressure / num_h ** 2)
+            * (pressure_i / num_h_i ** 2 + pressure / num_h ** 2)[:, np.newaxis]
             * grad_kernel,
             axis=0,
         )
@@ -48,11 +49,11 @@ def marangoni_force(V, num_h, surface_tension, num_h_i, st_i, grad_kernel):
         V**2
         / num_h_i
         * np.sum(
-            (surface_tension - st_i) / num_h * grad_kernel,
+            ((surface_tension - st_i) / num_h)[:, np.newaxis] * grad_kernel,
             axis=0,
         )
     )
 
 
 def viscosity_force(V, mu, uij, num_h, grad_kernel_reduced):
-    return V**2 * mu * np.sum(uij / num_h * grad_kernel_reduced)
+    return V**2 * mu * np.sum(uij / num_h[:, np.newaxis] * grad_kernel_reduced)

thin_film/render.py
@@ -3,15 +3,17 @@ import pdb
 import scipy
 from .color_system import cs_srgb, cmf
 
-# TODO: memory usage here is pretty high 
+# TODO: memory usage here is pretty high
 # because we're processing the frames all at once. makes more sense to chunk them
 # into manageable pieces since the pixels are independent
 
 # sample the height values on a grid
+
+
 def resample_heights(r, adv_h, res, bounds):
     # generate coordinates
     # TODO: can do this once instead of every frame
-    px, py = np.mgrid[0 : res[0] : 1, 0 : res[1] : 1]
+    px, py = np.mgrid[0: res[0]: 1, 0: res[1]: 1]
     px = (bounds[2] - bounds[0]) * px / res[0] + bounds[0]
     py = (bounds[3] - bounds[1]) * py / res[1] + bounds[1]
     points = np.c_[px.ravel(), py.ravel()]

thin_film/step.py
@@ -7,7 +7,7 @@ from .util import chunk
 
 
 def update_fields(r, u, Gamma, num_h, chunk, constants):
-    divergence = np.empty((chunk[1] - chunk[0], 1))
+    divergence = np.empty((chunk[1] - chunk[0],))
     curvature = np.empty_like(divergence)
     new_Gamma = np.empty_like(divergence)
 
@@ -43,7 +43,7 @@ def update_fields(r, u, Gamma, num_h, chunk, constants):
 
 def compute_forces(r, u, num_h, pressure, surface_tension, chunk, constants):
     force = np.zeros((chunk[1] - chunk[0], 2))
-    new_num_h = np.empty((chunk[1] - chunk[0], 1))
+    new_num_h = np.empty((chunk[1] - chunk[0],))
 
     for i in range(*chunk):
         rij = r - r[i]